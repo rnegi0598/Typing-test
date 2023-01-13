a0bfa17e40d31bd3fcfa40dfc9eadc1358cec901
rnegi0598 <rnegi0598@gmail.com>
2023-01-13 12:39:12 +0530

    added time duration and paragraph change option

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const paraText="King Dasaratha is about to announce that his Son Rama will be the next King. But Queen Kaikeyi wants her son to be king. A plot unfolds and the king faces a terrible dilemma.Read by Jana and featuring the voices of her Sister Cousins, Parveen and Elizabeth.For the past few days, workers had been scrubbing the stones of the Royal streets until they dazzled in the sunlight. And now soldiers in bright uniforms were lining the pavements and crowds were gathering behind them.The queen turned to him, and he wiped the tears off her face with a silk scarf, until, eventually, she said, “In my whole life, nobody has treated me with so much disrespect as you have done";
+let paraText='';
 
 const resetBtnEl=document.querySelector('.reset-div button');
 const timeEl=document.querySelector('.time');
@@ -9,41 +9,78 @@ const errorEl=document.querySelector('.errors span');
 const wpmEl=document.querySelector('.wpm span');
 const accuracyEl=document.querySelector('.accuracy span');
 const cpmEl=document.querySelector('.cpm span');
-let currentTime=60;
+let maxDuration=30; //default
+let currentTime=maxDuration;
 let timerId;
 let ind=0;
 let errors=0;
 let correct=0;
 let totalKeysPressed=0;
-
+let accuracy=0;
+let test=false;  //tell if test is going or not
+// update maxDuration 
+document.querySelector('.thirty-sec').onclick=function(){
+    console.log('30 clicked');
+    maxDuration=30;
+    timeEl.innerHTML='30';
+}
+document.querySelector('.sixty-sec').onclick=()=>{
+    console.log('60 clicked');
+    maxDuration=60;
+    timeEl.innerHTML='60';
+}
+//set paragraph
+function setPara(){
+    let ind=Math.floor(Math.random()*paragraph.length);
+    paraText=paragraph[ind];
+}
+setPara();
+// change paragraph
+document.querySelector('.change-para').onclick=()=>{
+    if(!test){
+        setPara();
+        resetText();
+        inputEl.value='';
+    }
+}
+//focus on input field on clicking on paragraph
+paraTextEl.onclick=()=>{
+    if(test){
+        inputEl.focus();
+    }
+       
+}
 function resetTimer(){
     
     //reset timer
-    currentTime=60;
+    currentTime=maxDuration;
     clearInterval(timerId);
     timerId=setInterval(()=>{
         if(currentTime<0){
-            clearInterval(timerId);
             inputEl.blur();
+            clearInterval(timerId);
             resetBtnEl.classList.remove('blur');
             resetBtnEl.innerHTML='Try Again';
             resetBtnEl.disabled=false;
-            notification.innerHTML='Click on Try Again button to begin the game.'
+            notification.innerHTML='Click on Try Again button to begin the test again.'
+            test=false;
             return ;
         }
         timeEl.innerHTML=`${currentTime}`;
         currentTime--;
         //update wpm
-        min=(60-currentTime)/60;
+        min=(maxDuration-currentTime)/60;
         words=totalKeysPressed/5;
         wpm=words/min;
         wpmEl.innerHTML=`${parseInt(wpm)}`;
         //update accuracy
-        var accuracy=(correct/totalKeysPressed)*100;
+        accuracy=(correct/totalKeysPressed)*100;
+        if(Object.is(NaN,accuracy)){
+            accuracy=0;
+        }
         accuracyEl.innerHTML=`${parseInt(accuracy)}`;
         //update cpm
-
-        cpmEl.innerHTML=`${parseInt(totalKeysPressed*(60-currentTime)/60)}`
+        cpmEl.innerHTML=`${parseInt(totalKeysPressed*(maxDuration-currentTime)/60)}`
         
     },1000);
 }
@@ -56,10 +93,14 @@ function resetText(){
     })
 }
 const resetApp=function(){
+    test=true;
+    setPara();
     inputEl.value='';
     ind=0;
     error=0;
+    accuracy=0;
     totalKeysPressed=0;
+    correct=0
     wpmEl.innerHTML=0;
     errorEl.innerHTML=0;
     timeEl.innerHTML=0;
@@ -69,8 +110,6 @@ const resetApp=function(){
     inputEl.focus();
     resetTimer();
     resetText();
-   
-
 
 };
 resetText();
@@ -86,10 +125,6 @@ inputEl.value='';
 // on typing 
 inputEl.addEventListener('input',(e)=>{
     totalKeysPressed++;
-    // min=(60-currentTime)/60;
-    // words=totalKeysPressed/5;
-    // wpm=words/min;
-    // wpmEl.innerHTML=`${parseInt(wpm)}`;
     let inputValue=inputEl.value;
     typedChar=inputValue[inputValue.length-1];
 
@@ -102,10 +137,12 @@ inputEl.addEventListener('input',(e)=>{
             return;
         }
         ind--;
+        //update errors
         if(spanEl[ind].classList.contains('incorrect')){
             errors--;
         }
         errorEl.innerHTML=`${errors}`;
+        //remove classses
         spanEl[ind].classList.remove('correct','incorrect','active');        
         return ;
     }
